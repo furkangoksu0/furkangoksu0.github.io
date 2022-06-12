28d75a08501dd20b9cdfd6cf42d1b03ab600baeb
Furkan GÖKSU <92264688+furkangoksu0@users.noreply.github.com>
2022-06-12 15:00:13 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+{
+    "cars":
+     [
+            {
+                "Proje" : "HTML BASİT HESAP MAKİNESİ",
+                "Tarih" : "20",
+                "Açıklama" : "https://github.com/furkangoksu0"
+                
+            },
+            {
+                "Proje" : "İNÖNÜ ÜNİVERSİTESİ TANITIM SAYFASI",
+                "Tarih" : "20",
+                "Açıklama" : "https://github.com/furkangoksu0"
+            },
+            {
+                "Proje" : "YIĞIT VE KUYRUK KULLANIMI",
+                "Tarih" : "20",
+                "Açıklama" : "https://github.com/furkangoksu0"
+            },
+            {
+                "Proje" : "C BAĞLI LİSTE",
+                "Tarih" : "20",
+                "Açıklama" : "https://github.com/furkangoksu0"
+            }    
+        ]
+}
